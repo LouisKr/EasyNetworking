1eb6f4db4f057f99097a5135c45a845e59efa58a
LouisKr <52064778+LouisKr@users.noreply.github.com>
2021-01-05 08:51:52 +0100

    Update Client.h

--- Client.h
+++ Client.h
@@ -2,17 +2,16 @@
 #include <iostream>
 #include <string>
 #include <WS2tcpip.h>
-
 #pragma comment(lib, "ws2_32.lib")
 
 class Client
 {
 public:
-    Client(std::string IP, int Port);
+    Client(std::string ServerIP, int ServerPort);
+
     bool Connect();
     bool Send(std::string msg);
     bool Clean();
-
     std::string Reciev();
 private:
     std::string mIP;
